2043c8d18c10889a5d925a233d8e60e90163cc16
Nikita Sviridov <n.sviridov14@gmail.com>
2018-09-12 10:34:02 +0500

    Свиридов Никита task05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -4,9 +4,15 @@ public class Task05 {
 
     public static String solution(int x) {
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "FALSE";
+        char[] stringNumber = Integer.toString(x).toCharArray();
+        for (char c : stringNumber)
+        {
+            if ((int) c % 2 !=0)
+            {
+                return "FALSE";
+            }
+        }
+        return "TRUE";
     }
 
     public static void main(String[] args) {
